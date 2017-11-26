338e992ad74c5de3b4d4e8746676160bf322989c
Ben Magee <ben@bmagee.com>
2017-11-26 13:58:55 +0000

    Generate output path from matched information

--- app.go
+++ app.go
@@ -27,9 +27,13 @@ var acronyms = map[string]string {
 	"tfpoba": "The Fresh Prince of Bel Air",
 	"satc": "Sex and The City",
 	"tbbt": "The Big Bang Theory",
-	"potc": "Pirates of The Carribbean",
+	"potc": "Pirates of The Caribbean",
+	"rots": "Return of The Sith",
+	"aotc": "Attack of The Clones",
 }
 
+var outputDirectoryFormat = "{Show}/Season {Season}/"
+var outputFileFormat = "{Show} - S{Season}E{Number} - {Episode}"
 
 func main() {
 	// Get input directory
@@ -56,6 +60,17 @@ func processDirectory(path string, f os.FileInfo, err error) error {
 		// Lookup successful
 		if err == nil {
 			fmt.Println(fmt.Sprintf("[tmdb] Show: %s, episode: %s, aired: %s, first seen: %s", result.Title, result.EpisodeName, result.AirDate, result.FirstAirDate))
+			// Generate output path
+			outputPath := strings.Replace(outputDirectoryFormat, "{Show}", result.Title, -1)
+			outputPath = strings.Replace(outputPath, "{Season}", fmt.Sprintf("%02d", result.SeasonNum), -1)
+
+			outputFile := strings.Replace(outputFileFormat, "{Show}", result.Title, -1)
+			outputFile = strings.Replace(outputFile, "{Episode}", result.EpisodeName, -1)			
+			outputFile = strings.Replace(outputFile, "{Season}", fmt.Sprintf("%02d", result.SeasonNum), -1)
+			outputFile = strings.Replace(outputFile, "{Number}", fmt.Sprintf("%02d", result.EpisodeNum), -1)		
+			outputFile += extension
+
+			fmt.Println(fmt.Sprintf("[r2d2] Generated output path: %s%s", outputPath, outputFile))
 		// Lookup failed
 		} else {
 			// Try swapping the matched episode title and series title
@@ -75,4 +90,8 @@ func processDirectory(path string, f os.FileInfo, err error) error {
 	time.Sleep(time.Second * 1)
 	
 	return nil
+}
+
+func generateTvOutputPath(lookup lookup.TvResult) string {
+	return ""
 }
